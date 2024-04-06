3a8b4e7d7e2f333a6a409f29dce892e9a9a6ab87
Rafael Luz <rafael@Nitro5-Rafael>
2024-04-06 17:57:29 -0300

    feat: Organiza as tipagens do componente post

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from './components/Header'
-import { Post } from './components/Post'
+import { Post, PostType } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
 
 import './global.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -17,7 +17,7 @@ const posts = [
     content: [
       { type: 'paragraph', content: 'Fala galeraa 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare'},
+      { type: 'link', content: 'jane.design/doctorcare' },
     ],
     publishedAt: new Date('2024-03-11 22:57:00')
   },
@@ -45,7 +45,7 @@ const posts = [
     content: [
       { type: 'paragraph', content: 'Fala galeraa 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare'},
+      { type: 'link', content: 'jane.design/doctorcare' },
     ],
     publishedAt: new Date('2024-03-09 22:57:00')
   }
@@ -63,9 +63,7 @@ export function App() {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
